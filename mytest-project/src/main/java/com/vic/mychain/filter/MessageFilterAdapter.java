819c7c235a93c178f733fe7192bdfdab9544a2bf
vicoqi <Vicxiaoqi@outlook.com>
2019-07-08 12:29:07 +0800

    commit mychain - channel

--- mytest-project/src/main/java/com/vic/mychain/filter/MessageFilterAdapter.java
+++ mytest-project/src/main/java/com/vic/mychain/filter/MessageFilterAdapter.java
@@ -2,12 +2,15 @@ package com.vic.mychain.filter;
 
 import com.vic.mychain.message.IMessage;
 
-public class MessageFilterAdapter<P extends IMessage> implements MessageFilter<P> {
+public class MessageFilterAdapter<P,R> implements MessageFilter<P,R> {
 
-    public void request(NextFilter<P> nextFilter, P request) throws Exception {
+    public void request(NextFilter nextFilter, IMessage request) throws Exception {
          nextFilter.request(request);
     }
 
+    public Class getNextClass(R  nexRequest) {
+        return nexRequest.getClass();
+    }
     public String toString() {
         return this.getClass().getSimpleName();
     }
